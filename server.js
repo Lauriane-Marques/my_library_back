aaf8eb1139eb1c66a7d54552bf386ddb756b3cb8
Lauriane-Marques <lauriane.marquesmartins@gmail.com>
2025-05-05 16:02:44 +0200

    [UPDATE] cors params

--- server.js
+++ server.js
@@ -13,7 +13,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static("public"));
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.post('/signup', authentification.signUp)
 app.post('/login', authentification.logIn)
