313873229adb00b5fe8de1c9f6e4171a5aa503ce
Lauriane-Marques <lauriane.marquesmartins@gmail.com>
2025-05-06 15:10:59 +0200

    [UPDATE] cors

--- server.js
+++ server.js
@@ -14,10 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static("public"));
 app.use(cors({
-  // origin: 'http://localhost:5173',
-  origin: '*',
-  // credentials: true,
-  // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  origin: 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
