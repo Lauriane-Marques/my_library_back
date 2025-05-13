c76594113287ba2d8e0b6ded25e41f5b9a166e27
Lauriane-Marques <lauriane.marquesmartins@gmail.com>
2025-05-13 10:15:08 +0200

    [UPDATE] cors config

--- server.js
+++ server.js
@@ -16,16 +16,50 @@ const userActions = require('./user/userActions');
 
 const app = express();
 
+const allowedOrigins = process.env.FRONTEND_URL 
+  ? process.env.FRONTEND_URL.split(',').map(origin => origin.trim())
+  : ['http://localhost:5173'];
+
+console.log('CORS origins:', allowedOrigins);
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static("public"));
+
+// app.use(cors({
+//   origin: config.frontendUrl,
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }));
+
 app.use(cors({
-  origin: config.frontendUrl,
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      console.log('Origine bloquée par CORS:', origin, 'Origines autorisées:', allowedOrigins);
+      callback(new Error('Non autorisé par CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
+//Debugging purpose
+app.use((req, res, next) => {
+  console.log('Requête reçue:', {
+    method: req.method,
+    path: req.path,
+    origin: req.headers.origin,
+    allowedOrigins
+  });
+  next();
+});
 
 app.post('/signup', authentification.signUp)
 app.post('/login', authentification.logIn)
